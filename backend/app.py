4371bcfc217e61ebaa4843b5fe5af200b138526b
frangu617 <142462584+frangu617@users.noreply.github.com>
2024-10-21 13:00:18 -0700

    can add, edit, and delete users

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///workout.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    height = db.Column(db.Float)
-    weight = db.Column(db.Float)
+    height = db.Column(db.Float)  # in cm
+    weight = db.Column(db.Float)  # in kg
     age = db.Column(db.Integer)
 
 class Workout(db.Model):
@@ -21,6 +23,63 @@ class Workout(db.Model):
     sets = db.Column(db.Integer)    # for weight training and bodyweight workouts
     reps = db.Column(db.Integer)    # for weight training and bodyweight workouts
     weight = db.Column(db.Float)    # for weight training
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = []
+    for user in users:
+        user_data = {
+            'id': user.id,
+            'name': user.name,
+            'height': user.height,
+            'weight': user.weight,
+            'age': user.age
+        }
+        users_list.append(user_data)
+    return jsonify( users_list)
+
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        user_data = {
+            'id': user.id,
+            'name': user.name,
+            'height': user.height,
+            'weight': user.weight,
+            'age': user.age
+        }
+        return jsonify(user_data)
+    else:
+        return jsonify({'message': 'User not found'})
+    
+    
+@app.route('/edit_user/<int:user_id>', methods=['PUT'])
+def edit_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        data = request.get_json()
+        user.name = data.get('name', user.name)
+        user.height = data.get('height', user.height)
+        user.weight = data.get('weight', user.weight)
+        user.age = data.get('age', user.age)
+        db.session.commit()
+        return jsonify({'message': 'User updated successfully'})
+    else:
+        return jsonify({'message': 'User not found'}), 404
+
+    
+    
+@app.route('/delete_user/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({'message': 'User deleted successfully'})
+    else:
+        return jsonify({'message': 'User not found'}), 404
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
